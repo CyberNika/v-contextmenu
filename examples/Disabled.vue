63ed490d031482c27871ab2cf148a815f23fedcb
snokier <me@snokier.com>
2017-11-05 13:20:10 +0800

    优化样式 & Submenu 添加 disables & 完善文档

---
+++ examples/Disabled.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="example">
+    <v-contextmenu ref="contextmenu">
+      <v-contextmenu-item @click="handleClick">菜单1</v-contextmenu-item>
+      <v-contextmenu-item disabled @click="handleClick">菜单2</v-contextmenu-item>
+
+      <v-contextmenu-item divider></v-contextmenu-item>
+
+      <v-contextmenu-submenu title="子菜单">
+        <v-contextmenu-item @click="handleClick">菜单3</v-contextmenu-item>
+
+        <v-contextmenu-item divider></v-contextmenu-item>
+
+        <v-contextmenu-submenu disabled title="子菜单">
+          <v-contextmenu-item @click="handleClick">菜单5</v-contextmenu-item>
+        </v-contextmenu-submenu>
+
+        <v-contextmenu-item @click="handleClick">菜单4</v-contextmenu-item>
+      </v-contextmenu-submenu>
+    </v-contextmenu>
+
+    <div class="box" v-contextmenu:contextmenu>
+      右键点击此区域
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Disabled',
+
+    methods: {
+      handleClick (event, vm) {
+        console.log(event, vm)
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  .box {
+    width: 100%;
+  }
+</style>
