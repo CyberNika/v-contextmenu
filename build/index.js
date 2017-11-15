b96b21df7cad377e362b03112ee811824d223408
CyberNika <cybernika.net@gmail.com>
2017-11-15 23:46:08 +0800

    fix #3

--- build/index.js
+++ build/index.js
@@ -6,5 +6,6 @@ const resolve = dir => path.resolve(__dirname, '..', dir)
 
 fs.emptyDirSync(resolve('dist'))
 fs.copySync(resolve('src'), resolve('dist'))
+fs.copySync(resolve('src/styles/fonts'), resolve('dist/fonts'))
 fs.removeSync(resolve('dist/styles'))
 execSync('./node_modules/stylus/bin/stylus src/styles/index.styl --out dist/')
