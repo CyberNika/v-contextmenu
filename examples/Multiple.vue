dfe88762acf028ef90e7af3a83de7991742e95b1
XBT1 <xbtop1@gmail.com>
2018-02-11 18:57:41 +0800

    feat: contextmenu 显示时隐藏其它实例

---
+++ examples/Multiple.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="example">
+    <v-contextmenu ref="contextmenu1" :theme="theme">
+      <v-contextmenu-item>菜单1</v-contextmenu-item>
+      <v-contextmenu-item>菜单2</v-contextmenu-item>
+      <v-contextmenu-item>菜单3</v-contextmenu-item>
+    </v-contextmenu>
+
+    <v-contextmenu ref="contextmenu2" :theme="theme">
+      <v-contextmenu-item>菜单1</v-contextmenu-item>
+      <v-contextmenu-item>菜单2</v-contextmenu-item>
+      <v-contextmenu-item>菜单3</v-contextmenu-item>
+    </v-contextmenu>
+
+    <div :class="['box', theme]" v-contextmenu:contextmenu1>
+      右键点击此区域
+    </div>
+
+    <div :class="['box', theme]" v-contextmenu:contextmenu2>
+      右键点击此区域
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Multiple',
+
+    props: {
+      theme: String,
+    },
+
+    methods: {},
+  }
+</script>
+
+<style scoped>
+  .box {
+    width: 100%;
+  }
+</style>
