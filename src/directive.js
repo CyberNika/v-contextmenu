550ad2ba64c6876d026a54dad31b33eee5ad2f81
XBT1 <xbtop1@gmail.com>
2018-03-12 19:54:32 +0800

    feat: 一个 ContextMenu 支持被多个 target 绑定 & 优化 contextmenuId

--- src/directive.js
+++ src/directive.js
@@ -2,7 +2,13 @@
 export default {
   bind (el, binding, vnode) {
     const contextmenu = vnode.context.$refs[binding.arg]
-    contextmenu.$refs.reference = el
-    contextmenu.$contextmenuId = binding.arg
+
+    if (Array.isArray(contextmenu.$refs.reference)) {
+      contextmenu.$refs.reference.push(el)
+    } else {
+      contextmenu.$refs.reference = [el]
+    }
+
+    contextmenu.$contextmenuId = el.id || contextmenu._uid // eslint-disable-line no-underscore-dangle
   },
 }
