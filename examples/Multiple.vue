86d090638cfeedcb6d084b94f4f760d6d94af7ff
XBTop1! <xbtop1@gmail.com>
2018-03-12 22:24:20 +0800

    feat: 一个 ContextMenu 支持被多个 target 绑定 & 优化 contextmenuId (#16)

--- examples/Multiple.vue
+++ examples/Multiple.vue
@@ -7,17 +7,22 @@
     </v-contextmenu>
 
     <v-contextmenu ref="contextmenu2" :theme="theme">
-      <v-contextmenu-item>菜单1</v-contextmenu-item>
-      <v-contextmenu-item>菜单2</v-contextmenu-item>
-      <v-contextmenu-item>菜单3</v-contextmenu-item>
+      <v-contextmenu-item>菜单2-1</v-contextmenu-item>
+      <v-contextmenu-item>菜单2-2</v-contextmenu-item>
+      <v-contextmenu-item>菜单2-3</v-contextmenu-item>
+      <v-contextmenu-item>菜单2-4</v-contextmenu-item>
     </v-contextmenu>
 
     <div :class="['box', theme]" v-contextmenu:contextmenu1>
-      右键点击此区域
+      右键点击此区域（ContextMenu1）
+    </div>
+
+    <div :class="['box', theme]" v-contextmenu:contextmenu1>
+      右键点击此区域（ContextMenu1）
     </div>
 
     <div :class="['box', theme]" v-contextmenu:contextmenu2>
-      右键点击此区域
+      右键点击此区域（ContextMenu2）
     </div>
   </div>
 </template>
