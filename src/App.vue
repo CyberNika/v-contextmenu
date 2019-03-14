38756fb616178ff480db14b39cf063138e890350
SuperStack <superstack.pro@outlook.com>
2019-03-14 23:11:35 +0800

    feat: 添加 package

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Contextmenu>
+      <div>111111</div>
+    </Contextmenu>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+
+import { Contextmenu } from "../package/index";
 
 @Component({
   components: {
-    HelloWorld,
+    Contextmenu,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
