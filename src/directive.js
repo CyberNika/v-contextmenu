f4ef1c6e8ca24c1195cf6569bd9922313529a8ef
何 <306905573@qq.com>
2019-09-18 11:50:27 +0800

    循环指令时，获取node错误 (#63)
    * 循环指令时，获取node错误
    
    * 升级版本，修复循环下组件获取node问题
    
    * directive adjustment:
    1. Modify array judgment

--- src/directive.js
+++ src/directive.js
@@ -2,8 +2,8 @@
 export default {
   // 之所以用 inserted 而不是 bind，是需要确保 contentmenu mounted 后才进行 addRef 操作
   inserted (el, binding, vnode) {
-    const contextmenu = vnode.context.$refs[binding.arg]
-
+    const node = vnode.context.$refs[binding.arg] || vnode.context.$refs[binding.value]
+    const contextmenu = Object.prototype.toString.call(node) === '[object Array]' ? node[0] : node
     contextmenu.addRef({ el, vnode })
     contextmenu.$contextmenuId = el.id || contextmenu._uid // eslint-disable-line no-underscore-dangle
   },
