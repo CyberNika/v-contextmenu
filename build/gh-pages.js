d7a764f1039dcf63ca01e1d41d91cd9b12ec4887
heynext <heynext.xyz@gmail.com>
2019-12-12 00:02:46 +0800

    reafctor: 更新了一些构建依赖和脚本

--- build/gh-pages.js
+++
@@ -1,5 +0,0 @@
-const ghpages = require('gh-pages')
-
-ghpages.publish('sites', (err) => {
-  err && console.log(err)
-})
--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,54 @@
-import vue from 'rollup-plugin-vue'
 import copy from 'rollup-plugin-copy'
-import uglify from 'rollup-plugin-uglify'
-import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import VuePlugin from 'rollup-plugin-vue'
+import { terser } from "rollup-plugin-terser";
 
 const config = {
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.common.js',
-    format: 'cjs',
-    strict: false,
-    exports: 'named',
-  },
   plugins: [
-    vue(),
-    babel(),
-    uglify(),
+    commonjs(),
+    VuePlugin(),
+    terser(),
     copy({
-      'src/styles/fonts': 'dist/fonts',
+      targets: [
+        { src: 'src/styles/fonts', dest: 'dist' },
+      ],
     }),
   ],
-}
+  external: ['vue-runtime-helpers'],
+};
 
-if (process.env.FORMAT === 'umd') {
-  config.output = Object.assign({}, config.output, {
-    file: 'dist/index.js',
-    format: 'umd',
-    name: 'VContextmenu',
-  })
-}
+export default [
+  // ESModule
+  Object.assign({}, config, {
+    output: {
+      file: 'dist/index.esm.js',
+      format: 'es',
+    },
+  }),
 
-export default config
+  // CommonJS
+  Object.assign({}, config, {
+    output: {
+      file: 'dist/index.common.js',
+      format: 'cjs',
+      strict: false,
+      exports: 'named',
+    },
+  }),
+
+  // UMD
+  Object.assign({}, config, {
+    output: {
+      file: 'dist/index.js',
+      format: 'umd',
+      name: 'VContextmenu',
+      strict: false,
+      exports: 'named',
+      globals: {
+        'vue-runtime-helpers': 'vueRuntimeHelpers',
+      },
+    },
+    plugins: config.plugins
+  }),
+];
