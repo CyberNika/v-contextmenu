f3534a65fda30bcc1ab582a6933b9f67220a5819
SuperStack <superstack.pro@outlook.com>
2020-07-19 23:45:56 +0800

    refactor: vue 3.0 & vite

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Custom Layout!</h1>
+  <Content/><!-- make sure to include markdown outlet -->
+</template>
--- package/utils.ts
+++
@@ -1,5 +0,0 @@
-export const is: {
-  [key: string]: (data: any) => boolean,
-} = {
-  number: (data: any) => typeof data === "number",
-};
