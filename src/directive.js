c365edce2f065b9bed7175cf87d97b8a7e82db9c
wangfukang <wangfukang@kuaishou.com>
2023-12-28 19:42:02 +0800

    fix: memory leak

--- src/directive.js
+++ src/directive.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-param-reassign */
 export default {
+  // 在组件 mount 前，设置 $contextmenuId，修复内存泄漏的问题
+  bind (el, binding, vnode) {
+    const node = vnode.context.$refs[binding.arg] || vnode.context.$refs[binding.value]
+    const contextmenu = Object.prototype.toString.call(node) === '[object Array]' ? node[0] : node
+    contextmenu.$contextmenuId = el.id || contextmenu._uid // eslint-disable-line no-underscore-dangle
+  },
   // 之所以用 inserted 而不是 bind，是需要确保 contentmenu mounted 后才进行 addRef 操作
   inserted (el, binding, vnode) {
     const node = vnode.context.$refs[binding.arg] || vnode.context.$refs[binding.value]
     const contextmenu = Object.prototype.toString.call(node) === '[object Array]' ? node[0] : node
     contextmenu.addRef({ el, vnode })
-    contextmenu.$contextmenuId = el.id || contextmenu._uid // eslint-disable-line no-underscore-dangle
   },
 }
