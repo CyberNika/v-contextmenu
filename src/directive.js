25d81d25b8fe8bdc9b2c0f9307c549aeb988a007
fukang wang <ssswfk@163.com>
2024-02-26 19:46:30 +0800

    fix: 增加 contextmenu 的空检查 (#143)
    Co-authored-by: wangfukang <wangfukang@kuaishou.com>

--- src/directive.js
+++ src/directive.js
@@ -4,12 +4,16 @@ export default {
   bind (el, binding, vnode) {
     const node = vnode.context.$refs[binding.arg] || vnode.context.$refs[binding.value]
     const contextmenu = Object.prototype.toString.call(node) === '[object Array]' ? node[0] : node
-    contextmenu.$contextmenuId = el.id || contextmenu._uid // eslint-disable-line no-underscore-dangle
+    if (contextmenu) {
+      contextmenu.$contextmenuId = el.id || contextmenu._uid // eslint-disable-line no-underscore-dangle
+    }
   },
   // 之所以用 inserted 而不是 bind，是需要确保 contentmenu mounted 后才进行 addRef 操作
   inserted (el, binding, vnode) {
     const node = vnode.context.$refs[binding.arg] || vnode.context.$refs[binding.value]
     const contextmenu = Object.prototype.toString.call(node) === '[object Array]' ? node[0] : node
-    contextmenu.addRef({ el, vnode })
+    if (contextmenu) {
+      contextmenu.addRef({ el, vnode })
+    }
   },
 }
